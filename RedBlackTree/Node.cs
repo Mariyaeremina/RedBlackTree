0afce7c7a4899f91efd2738c281968cdca0dce88
Mariyaeremina <Mariyaeremina@outlook.com>
2018-06-22 23:10:18 +0500

    initial commit

---
+++ RedBlackTree/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBlackTreeSolution
+{
+    public class Node<T>
+    {
+        public T Key;
+        public Node<T> LeftChild;
+        public Node<T> RightChild;
+        public Node<T> Parent;
+        public char Color;
+
+        public Node(T element, Node<T> parent)
+        {
+            Key = element;
+            Parent = parent;
+            Color = 'R';
+            LeftChild = null;
+            RightChild = null;
+        }
+    }
+}
+
---
+++ RedBlackTreeUnitTests/UnitTest1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RedBlackTreeSolution;
+
+namespace RedBlackTreeUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckInsertInteger()
+        {
+            RedBlackTree<int> tree = new RedBlackTree<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                tree.Insert(i);
+            }
+            Assert.AreEqual(0, tree.Find(0).Key);
+            Assert.AreEqual(4, tree.Find(4).Key);
+        }
+
+        [TestMethod]
+        public void CheckDeleteInteger()
+        {
+            RedBlackTree<int> tree = new RedBlackTree<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                tree.Insert(i);
+            }
+            tree.Delete(4);
+            Assert.AreEqual(null, tree.Find(4));
+        }
+
+        [TestMethod]
+        public void CheckInsertString()
+        {
+            RedBlackTree<string> tree = new RedBlackTree<string>();
+            tree.Insert("a");
+            tree.Insert("b");
+            tree.Insert("c");
+            tree.Insert("d");
+            Assert.AreEqual("a", tree.Find("a").Key);
+            Assert.AreEqual("d", tree.Find("d").Key);
+        }
+
+        [TestMethod]
+        public void CheckDeleteString()
+        {
+            RedBlackTree<string> tree = new RedBlackTree<string>();
+            tree.Insert("a");
+            tree.Insert("b");
+            tree.Insert("c");
+            tree.Insert("d");
+            tree.Delete("d");
+            Assert.AreEqual(null, tree.Find("d"));
+        }
+
+        [TestMethod]
+        public void CheckCleansingTree()
+        {
+            RedBlackTree<int> tree = new RedBlackTree<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                tree.Insert(i);
+            }
+            Assert.AreEqual(false, tree.IsEmpty());
+            tree.Clear();
+            Assert.AreEqual(true, tree.IsEmpty());
+        }
+
+        [TestMethod]
+        public void CheckSearching()
+        {
+            RedBlackTree<int> tree = new RedBlackTree<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                tree.Insert(i);
+            }
+            Assert.AreEqual(7, tree.FindNext(6).Key);
+            Assert.AreEqual(5, tree.FindPrevious(6).Key);
+            Assert.AreEqual(null, tree.FindNext(9));
+            Assert.AreEqual(0, tree.FindPrevious(0).Key);
+        }
+
+        public void CheckInserting()
+        {
+            RedBlackTree<int> tree = new RedBlackTree<int>();
+            for (int i = 0; i < 3; i++)
+            {
+                tree.Insert(i);
+            }
+            Assert.AreEqual(1, tree.Root.Key);
+            Assert.AreEqual(0, tree.Root.LeftChild.Key);
+            Assert.AreEqual(2, tree.Root.RightChild.Key);
+        }
+
+        public void CheckColoring()
+        {
+            RedBlackTree<int> tree = new RedBlackTree<int>();
+            for (int i = 0; i < 3; i++)
+            {
+                tree.Insert(i);
+            }
+            Assert.AreEqual("B", tree.Root.Color);
+            Assert.AreEqual("R", tree.Root.LeftChild.Color);
+            Assert.AreEqual("R", tree.Root.RightChild.Color);
+        }
+    }
+}
